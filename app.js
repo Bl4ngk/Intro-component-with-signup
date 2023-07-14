fbece51724425fec7f2b4d45a73597764623a384
Bl4ngk <atmxtwo@gmail.com>
2023-07-14 19:09:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const inputs = document.querySelectorAll('.form--input');
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event =>{
+    event.preventDefault();
+    console.log('Submit');
+    inputs.forEach(input => {
+    const curSpan = input.nextElementSibling;
+    const curParent = input.parentElement;
+    // console.log(input.name)
+    // console.log(!input.value)
+    if(!input.value && input.name !== 'email'){
+        console.log(input.name )
+        input.classList.add('error-img');
+        curSpan.classList.remove('hidden');
+        curParent.style.marginBottom = '7px';
+    }
+    else if(input.name === 'email' && !validateEmail(input.value)){
+        curSpan.classList.remove('hidden');
+        curParent.style.marginBottom = '7px';
+        curSpan.textContent = 'Email cannot be empty';
+        input.classList.add('error-img');
+        if(input.value !==''){
+            curSpan.textContent = 'Looks like this is not an email';
+        }
+    }
+    else{
+        curSpan.classList.add('hidden');
+        input.classList.remove('error-img');
+        input.parentElement.style.marginBottom = '24px';
+    }    
+    });
+});
+        
+        
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
